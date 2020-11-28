ec0c1b6ee8777de05e4dd69850bf8fbb38e5c074
aadesh goyal <goyalaadesh462@gmail.com>
2020-11-28 16:59:59 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => {
+    console.log('listening on port ' + port);
+});
