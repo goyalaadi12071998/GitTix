c1248619c198f4c00e182ff65fde114649615512
aadesh goyal <goyalaadesh462@gmail.com>
2020-11-28 17:34:07 +0530

    Cloud database added

--- src/index.js
+++ src/index.js
@@ -3,9 +3,27 @@ const app = require('./app');
 const port = process.env.PORT || 8080;
 
 async function start(){
-    app.listen(port, () => {
-        console.log('Listening on port ' + port);
-    });
+    if(process.env.NODE_ENV === 'production') {
+        if(!process.env.MONGODB_URI){
+            throw new Error('MONGODB_URI must be defined');
+        }
+    }
+
+    try{
+
+        await mongoose.connect(process.env.MONGODB_URI,{
+            useCreateIndex: true,
+            useUnifiedTopology: true,
+            useNewUrlParser: true
+        });
+
+        app.listen(port, () => {
+            console.log('Listening on port ' + port);
+        });
+
+    }catch(err){
+        throw new Error(err);
+    }
 }
 
 start();
