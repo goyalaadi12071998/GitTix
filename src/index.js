37ed1df4815fce2e0390cb2ad1755a6391be33a7
aadesh goyal <goyalaadesh462@gmail.com>
2020-11-28 17:01:44 +0530

    second commit

--- src/index.js
+++ src/index.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
+app.get('/hello', (req, res) => {
+    res.send('Hello World!!!!!!!!!!!!!!!!!');
+})
+
 const port = process.env.PORT || 8080;
 
 app.listen(port, () => {
