ac77c4af734ca8f66edb24e22bfebdef556fb7cc
aadesh goyal <goyalaadesh462@gmail.com>
2020-12-01 01:35:04 +0530

    securing my website

--- src/app.js
+++ src/app.js
@@ -51,7 +51,7 @@ if(nodeVersionMajor < 12){
 function ensureSecure(req, res, next) {
     //Heroku stores the origin protocol in a header variable. The app itself is isolated within the dyno and all request objects have an HTTP protocol.
     if (req.get('X-Forwarded-Proto')=='https' || req.hostname == 'localhost') {
-        //Serve Angular App by passing control to the next middleware
+        //Serve Node.js App by passing control to the next middleware
         next();
     } else if(req.get('X-Forwarded-Proto')!='https' && req.get('X-Forwarded-Port')!='443'){
         //Redirect if not HTTP with original request URL
@@ -61,6 +61,7 @@ function ensureSecure(req, res, next) {
 
 app.get('*',(req, res, next) => {
     ensureSecure(req, res, next);
+    next();
 });
 
 app.use(authRoutes);
